4ab400f017681e21889ef78bebacb055c2f38f18
fjand5 <huynhtheluat@gmail.com>
2021-10-14 18:41:13 +0700

    del

--- esp32/bai_1/main/hello_world_main.c
+++ esp32/bai_1/main/hello_world_main.c
@@ -1,25 +1,39 @@
 #include "esp_log.h"
 #include "esp_system.h"
 static const char *TAG = "main";
-void LOG_TO_STRING(const char *fmt, va_list _args)
+void myLogger(const char *str, va_list arg)
 {
-   va_list args = _args;
-   
-   
-   printf("\033[96;1m my log: ");
-   int size_string = vsnprintf(NULL, 0, fmt, args);
-   char *string = (char *)malloc(size_string);
-   vsnprintf(string, size_string, fmt, args);
-   printf(string);
-   printf("\033[0m\n");
-   // return vprintf(fmt, args);
+   printf("\033[35;1mLog của tui\033[0m\n");
+
+   int mIndex = 0;
+   bool isCL = false;
+   if (str[0] == '\033')
+   {
+      isCL = true;
+   }
+   while (str[mIndex] != 0 && str[mIndex] != 'm')
+   {
+      mIndex++;
+   }
+   mIndex++;
+   int len = vsnprintf(NULL, 0, str, arg);
+   char *data; 
+   if (isCL)
+   {
+      data = (char *)malloc(len - mIndex + 1);
+      vsnprintf(data, len, str + mIndex, arg);
+   }else{
+      data = (char *)malloc(len + 2);
+      vsnprintf(data, len + 1, str, arg);
+   }
+   printf(data);
 }
 void app_main(void)
 {
-   esp_log_set_vprintf(&LOG_TO_STRING);
-   ESP_LOGE(TAG, "ESP_LOGE %d\n", 1);
-   ESP_LOGW(TAG, "ESP_LOGW %d\n", 2);
-   ESP_LOGI(TAG, "ESP_LOGI %d\n", 3);
-   ESP_LOGD(TAG, "ESP_LOGD %d\n", 4);
-   ESP_LOGV(TAG, "ESP_LOGV %d\n", 5);
+   esp_log_set_vprintf(myLogger);
+   ESP_LOGE(TAG, "ESP_LOGE");
+   ESP_LOGW(TAG, "ESP_LOGW");
+   ESP_LOGI(TAG, "ESP_LOGI");
+   ESP_LOGD(TAG, "ESP_LOGD");
+   ESP_LOGV(TAG, "ESP_LOGV");
 }
